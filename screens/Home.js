87ac0646f79911302a0caefff593666bc5df79cf
Bartosz Soltysik <b.soltysik90@gmail.com>
2022-05-19 21:24:33 +0200

    components and constants

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,16 @@
-import { Text } from "react-native-web";
+import { useState } from "react";
+import { View, SafeArea, FlatList, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { COLORS, NFTData } from "../constants";
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
 const Home = () => {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+    </SafeAreaView>
+  );
 };
 
 export default Home;
