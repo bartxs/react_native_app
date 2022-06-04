6472fed0d4db36ea325d613757dc25c361e1ea20
Bartosz Soltysik <b.soltysik90@gmail.com>
2022-06-04 13:21:57 +0200

    NFTcard update

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -3,8 +3,9 @@ import { useNavigation } from "@react-navigation/native";
 
 import { COLORS, SIZES, SHADOWS, assets } from "../constants";
 import { CircleButton, RectButton } from "./Button";
+import { SubInfo, EthPrice, NFTTitle } from "./SubInfo";
 
-const NFTCard = () => {
+const NFTCard = ({ data }) => {
   const navigation = useNavigation();
 
   return (
@@ -29,6 +30,8 @@ const NFTCard = () => {
         />
         <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
+
+      <SubInfo />
     </View>
   );
 };
